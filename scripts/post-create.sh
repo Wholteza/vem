6236f9bb53ee4d8d9aa1a55605b5961b5a1b1a3e
wholteza <zackarias@montell.se>
2024-08-24 20:15:55 +0000

    Added automatic application of migrations

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,3 +1,5 @@
 dotnet dev-certs https
 dotnet user-secrets init --project Vem/Vem.csproj
 dotnet user-secrets set --project Vem/Vem.csproj "Postgresql:ConnectionString" "Server=db;Port=5432;Database=postgres;User Id=postgres;Password=Password1234!;"
+dotnet tool install --global dotnet-ef
+dotnet ef database update --project Vem/Vem.csproj
