59b14c74cdd76ee099503b9539778c2941744e31
wholteza <zackarias@montell.se>
2024-09-10 17:04:31 +0000

    Swapped incorrectly named files

--- Vem/Controllers/IdentityController.cs
+++ Vem/Controllers/IdentityController.cs
@@ -1,115 +1,57 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Vem.Controllers.Models.Requests;
-using Vem.Controllers.Models.Response;
 using Vem.Database.Contexts;
 using Vem.Database.Models;
-using Vem.Options;
 
 namespace Vem.Controllers;
 
 [ApiController]
-public class AuthenticationController : ControllerBase
+public class IdentityController : ControllerBase
 {
   private readonly IdentityContext identityContext;
   private readonly ApplicationSettingsContext applicationSettingsContext;
-  private readonly IOptions<AuthenticationOptions> authenticationOptions;
 
-  public AuthenticationController(IdentityContext identityContext, ApplicationSettingsContext applicationSettingsContext, IOptions<AuthenticationOptions> authenticationOptions)
+  public IdentityController(IdentityContext identityContext, ApplicationSettingsContext applicationSettingsContext)
   {
     this.identityContext = identityContext;
     this.applicationSettingsContext = applicationSettingsContext;
-    this.authenticationOptions = authenticationOptions;
+  }
+
+  [HttpGet]
+  [Route("identity/admin/initialized-status")]
+  [ProducesResponseType<string>(StatusCodes.Status200OK)]
+  public IActionResult IsInitialized()
+  {
+    return Ok(applicationSettingsContext.AdminAccountInitialized);
   }
 
   [HttpPost]
-  [Route("authentication/login")]
+  [Route("identity/admin")]
   [ProducesResponseType<string>(StatusCodes.Status200OK)]
-  public async Task<IActionResult> Login(PasswordAuthentication requestModel)
+  public async Task<IActionResult> Create(CreateIdentity requestModel)
   {
-    var identity = await identityContext.Identities.FirstOrDefaultAsync(identity => identity.Email == requestModel.Email);
-    if (identity is null || string.IsNullOrEmpty(requestModel.Password)) return BadRequest("Invalid email or password");
-    var isValid = identityContext.ValidatePassword(identity, requestModel.Password);
+    if (applicationSettingsContext.AdminAccountInitialized) return BadRequest("Admin account already initialized");
 
-    return Ok(GenerateJwtToken(identity));
-  }
+    using var transaction = await identityContext.Database.BeginTransactionAsync();
+    await applicationSettingsContext.Database.UseTransactionAsync(transaction.GetDbTransaction());
 
-  [HttpPost]
-  [Route("authentication/validate")]
-  [ProducesResponseType<string>(StatusCodes.Status200OK)]
-  public IActionResult Validate()
-  {
-    string authHeader = Request.Headers["Authorization"].FirstOrDefault() ?? "";
-    var claimsPricipal = ValidateJwtToken(authHeader.Replace("Bearer ", ""));
-    return Ok(new TokenValidation
+    var identity = await identityContext.CreateIdentityWithPasswordAuthentication(new Identity
     {
-      IsValid = claimsPricipal is not null,
-      ValidUntil = claimsPricipal?.Claims.FirstOrDefault(claim => claim.Type == "exp")?.Value is not null ? new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(long.Parse(claimsPricipal.Claims.FirstOrDefault(claim => claim.Type == "exp")?.Value)) : null,
-      IssuedAt = claimsPricipal?.Claims.FirstOrDefault(claim => claim.Type == "iat")?.Value is not null ? new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(long.Parse(claimsPricipal.Claims.FirstOrDefault(claim => claim.Type == "iat")?.Value)) : null
-    });
-  }
+      FirstName = requestModel.FirstName,
+      LastName = requestModel.LastName,
+      Nickname = requestModel.Nickname,
+      Email = requestModel.Email,
+      IsAdmin = true
+    }, requestModel.Password);
 
-  private string GenerateJwtToken(Identity identity)
-  {
-    // Define token handler
-    var tokenHandler = new JwtSecurityTokenHandler();
+    applicationSettingsContext.AdminAccountInitialized = true;
 
-    if (authenticationOptions.Value.Secret is null) throw new Exception("Secret not set");
+    identityContext.SaveChanges();
 
-    // Define a secret key for signing the token
-    var key = Encoding.ASCII.GetBytes(authenticationOptions.Value.Secret); // Replace with your secret key
+    await transaction.CommitAsync();
 
-    // Define token descriptor
-    var tokenDescriptor = new SecurityTokenDescriptor
-    {
-      Subject = new ClaimsIdentity(
-        [
-                new Claim(ClaimTypes.Name, $"{identity.FirstName} {identity.LastName}"),
-                new Claim(ClaimTypes.Role, identity.IsAdmin ? "Admin" : "User")
-            ]),
-      Expires = DateTime.UtcNow.AddHours(1),
-      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-    };
-
-    // Create the token
-    var token = tokenHandler.CreateToken(tokenDescriptor);
-
-    // Return the serialized token
-    return tokenHandler.WriteToken(token);
-  }
-
-  private ClaimsPrincipal? ValidateJwtToken(string token)
-  {
-    var tokenHandler = new JwtSecurityTokenHandler();
-    if (authenticationOptions.Value.Secret is null) throw new Exception("Secret not set");
-    var key = Encoding.ASCII.GetBytes(authenticationOptions.Value.Secret); // Replace with your secret key
-
-    var validationParameters = new TokenValidationParameters
-    {
-      ValidateIssuerSigningKey = true,  // Ensure the token's signature is valid
-      IssuerSigningKey = new SymmetricSecurityKey(key),  // Provide the same key used to generate the token
-      ValidateIssuer = false,           // (Optional) Specify whether to validate the token's issuer
-      ValidateAudience = false,         // (Optional) Specify whether to validate the token's audience
-      ValidateLifetime = true,          // Ensure the token hasn't expired
-      ClockSkew = TimeSpan.Zero         // Time tolerance to account for potential clock differences
-    };
-
-    // Validate the token and return the principal (claims)
-    SecurityToken validatedToken;
-    try
-    {
-      ClaimsPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
-      return principal;
-    }
-    catch (SecurityTokenExpiredException)
-    {
-      return null;
-    }
+    return Ok(identity);
   }
 }
